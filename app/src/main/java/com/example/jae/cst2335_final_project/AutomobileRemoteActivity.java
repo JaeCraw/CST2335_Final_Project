8b5276d005a3b4c046a627e7df6a9bf19afda028
bmohm90 <bmohm90@gmail.com>
2016-11-24 17:03:25 -0500

    Added the drawable folder and updated my xml file

--- app/src/main/java/com/example/jae/cst2335_final_project/AutomobileRemoteActivity.java
+++ app/src/main/java/com/example/jae/cst2335_final_project/AutomobileRemoteActivity.java
@@ -2,8 +2,12 @@ package com.example.jae.cst2335_final_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class AutomobileRemoteActivity extends AppCompatActivity {
+
+    protected static final String ACTIVITY_NAME = "AutomobileRemoteActivity";
+    ListView automobileListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
